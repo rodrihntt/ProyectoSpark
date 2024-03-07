3b042f3bf12513c22ea96f5c9045309f1e0747bf
Vespertino <Vespertino@C14PC08.informatica.comercio>
2024-03-07 16:52:03 +0100

    datos agregados

--- InsertarDatosBD.py
+++ InsertarDatosBD.py
@@ -1,6 +1,7 @@
 import psycopg2
 import random
 from faker import Faker
+import numpy as np
 
 # Configuración de conexión a la base de datos
 connection = psycopg2.connect(
@@ -14,22 +15,43 @@ connection = psycopg2.connect(
 # Crear un cursor para ejecutar comandos SQL
 cursor = connection.cursor()
 
+# Crear la tabla "Stores" si no existe
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Stores (
+        store_id SERIAL PRIMARY KEY,
+        store_name VARCHAR(255),
+        location VARCHAR(255),
+        demographics VARCHAR(255)
+    )
+''')
+
+# Confirmar la creación de la tabla
+connection.commit()
+
 # Crear datos de manera aleatoria usando Faker
 fake = Faker()
 
 # Número de filas a insertar
-num_filas = 10
+num_filas = 50
 
 # Insertar datos en la tabla "Stores"
 for _ in range(num_filas):
     store_name = fake.company()
     location = fake.address()
-    demographics = {"age_group": random.choice(["18-25", "26-35", "36-45"]),
-                   "income_range": random.choice(["Low", "Medium", "High"])}
+    demographics = random.choice(["EEUU", "Alemania", "España"])
 
     # Insertar datos en la tabla
     cursor.execute("INSERT INTO Stores (store_name, location, demographics) VALUES (%s, %s, %s)",
                    (store_name, location, demographics))
+
+# Datos fake
+for _ in range(10):
+    store_name = np.nan
+    location = " "
+    demographics = random.choice([123, 865, 888])
+
+    cursor.execute("INSERT INTO Stores (store_name, location, demographics) VALUES (%s, %s, %s)",
+                (store_name, location, demographics))
 
 # Confirmar y cerrar la conexión
 connection.commit()

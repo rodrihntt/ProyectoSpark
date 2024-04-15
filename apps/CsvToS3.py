d7716dbdc0d4851d2ae49191b2c2d84de481adfa
Vespertino <Vespertino@C14PC08.informatica.comercio>
2024-04-15 20:50:30 +0200

    cambios

--- apps/CsvToS3.py
+++ apps/CsvToS3.py
@@ -11,9 +11,12 @@ spark = SparkSession.builder \
     .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
     .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
     .config("spark.sql.shuffle.partitions", "4") \
+    .config("spark.jars.packages","org.apache.spark:spark-hadoop-cloud_2.13:3.5.1,software.amazon.awssdk:s3:2.25.11") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .master("local[*]") \
+    .config("spark.driver.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
+    .config("spark.executor.extraClassPath", "/opt/spark/jars/s3-2.25.11.jar") \
+    .master("spark://spark-master:7077") \
     .getOrCreate()
 
 try:
@@ -22,11 +25,11 @@ try:
     
     # Escribir el DataFrame en S3
     df.write \
-        .option('fs.s3a.committer.name', 'directory') \
+        .option('fs.s3a.committer.name', 'partitioned') \
         .option('fs.s3a.committer.staging.conflict-mode', 'replace') \
         .option("fs.s3a.fast.upload.buffer", "bytebuffer") \
         .mode('overwrite') \
-        .csv(path='s3a://bucket-rodrigo/sales_data.csv')
+        .csv(path='s3a://bucket-rodrigo/sales_data.csv', sep=',')
 
     spark.stop()
     
